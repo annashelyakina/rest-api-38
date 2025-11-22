9ddce9596b8b1ff8180ca1d8ade82dbb6ab4a0b4
annashelyakina <anna.shelyakina82@gmail.com>
2025-11-22 18:46:57 +0300

    added Constants

---
+++ src/test/java/in/reqres/Constants.java
@@ -0,0 +1,16 @@
+package in.reqres;
+
+import io.restassured.http.Header;
+
+public class Constants {
+    private Constants() {}
+    public static final String baseUrl = "https://reqres.in/api";
+    public static final Header validApiKey = new Header("x-api-key", "reqres-free-v1");
+    public static final Header invalidApiKey = new Header("x-api-key", "reqres-free-v111");
+    public static final String validCreateData = "{\"name\": \"Izabel\", \"job\": \"engineer\"}";
+    public static final String validRegisterData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\"}";
+    public static final String invalidEmailInRegisterData = "{\"email\": \"eve111.holt@reqres.in\", \"password\": \"pistol\"}";
+    public static final String onlyPasswordInRegisterData = "{\"password\": \"pistol\"}";
+    public static final String onlyEmailInRegisterData = "{\"email\": \"eve.holt@reqres.in\"}";
+
+}
