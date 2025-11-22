f1945789268a38cbbfb282d5de3068be0465fe47
annashelyakina <anna.shelyakina82@gmail.com>
2025-11-22 23:46:34 +0300

    updated GetUserTests

--- src/test/java/in/reqres/GetUserTests.java
+++ src/test/java/in/reqres/GetUserTests.java
@@ -4,17 +4,25 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class GetUserTests {
+public class GetUserTests extends TestBase{
+    public static final String apiKey = "reqres-free-v1";
+
     @Test
     @DisplayName("Проверка получения данных для существующего пользователя")
     void successfulGetUserTest() {
+        // Предварительная проверка токена
+        assertThat(apiKey).isNotBlank();   // Должен быть непустой строкой
+        assertThat(apiKey.length()).isGreaterThanOrEqualTo(14);  // Минимальная длина должна быть > = 14 символов
+        assertThat(apiKey).matches("[a-zA-Z0-9-]+");     // Должны быть алфавитно-цифровые символы и дефис
+
         given()
-                .header(Constants.validApiKey)
-                 .log().uri()
+                .header("x-api-key", apiKey)
+                .log().uri()
                 .when()
-                .get(Constants.baseUrl + "/users/2")
+                .get("/users/2")
                 .then()
                 .log().status()
                 .log().body()
@@ -26,11 +34,16 @@ public class GetUserTests {
     @Test
     @DisplayName("Проверка статус кода в случае, если пользователь не найден")
     void notFoundUserTest() {
+        // Предварительная проверка токена
+        assertThat(apiKey).isNotBlank();   // Должен быть непустой строкой
+        assertThat(apiKey.length()).isGreaterThanOrEqualTo(14);  // Минимальная длина должна быть больше 8 символов
+        assertThat(apiKey).matches("[a-zA-Z0-9-]+");     // Только алфавитно-цифровые символы и дефис
+
         given()
                 .header(Constants.validApiKey)
                 .log().uri()
                 .when()
-                .get(Constants.baseUrl + "/users/23")
+                .get("/users/23")
                 .then()
                 .log().status()
                 .log().body()
