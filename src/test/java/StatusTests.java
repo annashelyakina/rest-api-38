18e67e4606d12993ac413e6cddb1724248da5ec6
annashelyakina <anna.shelyakina82@gmail.com>
2025-11-22 11:35:39 +0300

    added StatusTests

---
+++ src/test/java/StatusTests.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class StatusTests {
+  /*
+  1. Make request to https://selenoid.autotests.cloud/status
+  2. Get response { total: 5, used: 0, queued: 0, pending: 0, browsers:...
+  3. Check total is 20
+   */
+
+    @Test
+    void checkTotal5() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(5));
+
+
+    }
+
+    @Test
+    void checkTotalWithResponseLogs() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total", is(5));
+    }
+
+    @Test
+    void checkTotalWithLogs() {
+        given()
+                .log().all()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total", is(5));
+    }
+}
