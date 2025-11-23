8730122a6a3b5a5fac98bd0bcf91a85c67b2c1d7
annashelyakina <anna.shelyakina82@gmail.com>
2025-11-23 13:28:28 +0300

    updated GetUserTests

--- src/test/java/in/reqres/GetUserTests.java
+++ src/test/java/in/reqres/GetUserTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class GetUserTests extends TestBase{
@@ -13,13 +12,8 @@ public class GetUserTests extends TestBase{
     @Test
     @DisplayName("Проверка получения данных для существующего пользователя")
     void successfulGetUserTest() {
-        // Предварительная проверка токена
-        assertThat(apiKey).isNotBlank();   // Должен быть непустой строкой
-        assertThat(apiKey.length()).isGreaterThanOrEqualTo(14);  // Минимальная длина должна быть > = 14 символов
-        assertThat(apiKey).matches("[a-zA-Z0-9-]+");     // Должны быть алфавитно-цифровые символы и дефис
-
-        given()
-                .header("x-api-key", apiKey)
+            given()
+                .header(Constants.validApiKey)
                 .log().uri()
                 .when()
                 .get("/users/2")
@@ -34,12 +28,7 @@ public class GetUserTests extends TestBase{
     @Test
     @DisplayName("Проверка статус кода в случае, если пользователь не найден")
     void notFoundUserTest() {
-        // Предварительная проверка токена
-        assertThat(apiKey).isNotBlank();   // Должен быть непустой строкой
-        assertThat(apiKey.length()).isGreaterThanOrEqualTo(14);  // Минимальная длина должна быть больше 8 символов
-        assertThat(apiKey).matches("[a-zA-Z0-9-]+");     // Только алфавитно-цифровые символы и дефис
-
-        given()
+          given()
                 .header(Constants.validApiKey)
                 .log().uri()
                 .when()
