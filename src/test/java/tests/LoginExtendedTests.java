33c84639e2e9b56a30ae9a39959fcca28e133e2d
annashelyakina <anna.shelyakina82@gmail.com>
2025-11-25 23:03:32 +0300

    added allure

--- src/test/java/tests/LoginExtendedTests.java
+++ src/test/java/tests/LoginExtendedTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import models.lombok.LoginBodyLombokModel;
@@ -8,6 +9,8 @@ import models.pojo.LoginBodyModel;
 import models.pojo.LoginResponseModel;
 import org.junit.jupiter.api.Test;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,6 +36,8 @@ public class LoginExtendedTests {
                 .body(authData)
                 .contentType(ContentType.JSON)
                 .log().uri()
+                .log().body()
+                .log().headers()
 
         .when()
               .post("https://reqres.in/api/login")
@@ -55,6 +60,8 @@ public class LoginExtendedTests {
                 .body(authData)
                 .contentType(ContentType.JSON)
                 .log().uri()
+                .log().body()
+                .log().headers()
 
         .when()
                 .post("https://reqres.in/api/login")
@@ -79,6 +86,8 @@ public class LoginExtendedTests {
                 .body(authData)
                 .contentType(ContentType.JSON)
                 .log().uri()
+                .log().body()
+                .log().headers()
 
                 .when()
                 .post("https://reqres.in/api/login")
@@ -90,5 +99,88 @@ public class LoginExtendedTests {
                 .extract().as(LoginResponseLombokModel.class);
 
         assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+    }
+
+    @Test
+    void successfulLoginAllureTest() {
+        LoginBodyLombokModel authData = new LoginBodyLombokModel();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+
+        LoginResponseLombokModel response = given()
+                .filter(new AllureRestAssured())
+                .log().uri()
+                .log().body()
+                .log().headers()
+                .header("x-api-key", "reqres-free-v1")   // Передаем правильный объект заголовка
+                .body(authData)
+                .contentType(ContentType.JSON)
+
+        .when()
+                .post("https://reqres.in/api/login")
+
+        .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModel.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+    }
+
+    @Test
+    void successfulLoginCustomAllureTest() {
+        LoginBodyLombokModel authData = new LoginBodyLombokModel();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+
+        LoginResponseLombokModel response = given()
+                .filter(withCustomTemplates())
+                .log().uri()
+                .log().body()
+                .log().headers()
+                .header("x-api-key", "reqres-free-v1")   // Передаем правильный объект заголовка
+                .body(authData)
+                .contentType(ContentType.JSON)
+
+                .when()
+                .post("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModel.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+    }
+
+    @Test
+    void successfulLoginWithStepsTest() {
+        LoginBodyLombokModel authData = new LoginBodyLombokModel();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+
+        LoginResponseLombokModel response = step("Make request", ()->
+           given()
+                .filter(withCustomTemplates())
+                .log().uri()
+                .log().body()
+                .log().headers()
+                .header("x-api-key", "reqres-free-v1")   // Передаем правильный объект заголовка
+                .body(authData)
+                .contentType(ContentType.JSON)
+
+                .when()
+                .post("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModel.class));
+
+        step("Check response", ()->
+            assertEquals("QpwL5tke4Pnpja7X4", response.getToken()));
     }
 }
